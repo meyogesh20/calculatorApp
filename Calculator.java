5969bd5028fed17f1e3e9386a5d0ad5b14e2a1ee
Yogesh Mahajan <meyogesh20@gmail.com>
2022-06-21 09:47:58 +0530

    first draft Code

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+    }
+
+    public int add(int num1, int num2){
+        int sum=num1+num2;
+        return sum;
+    }
+
+}
